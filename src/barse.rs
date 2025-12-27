1c4d2aab54625adb68b676e61312b00b8acabdda
Clayton Knittel <cknit1999@gmail.com>
2025-12-26 20:59:51 -0800

    box the map

--- src/barse.rs
+++ src/barse.rs
@@ -6,20 +6,22 @@ use crate::{
   error::BarseResult, scanner::Scanner, str_hash::BuildStringHash, table::WeatherStationTable,
 };
 
+const TABLE_SIZE: usize = 16384;
+
 unsafe fn round_up_to_32b_boundary(buffer: &[u8]) -> &[u8] {
   unsafe { slice::from_raw_parts(buffer.as_ptr(), buffer.len().next_multiple_of(32)) }
 }
 
 pub fn build_temperature_reading_table(
   input_path: &str,
-) -> BarseResult<WeatherStationTable<16384, BuildStringHash>> {
+) -> BarseResult<Box<WeatherStationTable<TABLE_SIZE, BuildStringHash>>> {
   let file = File::open(input_path)?;
   let map = unsafe { MmapOptions::new().map(&file) }?;
   map.advise(Advice::Sequential)?;
   let map_buffer = unsafe { round_up_to_32b_boundary(&map) };
 
   Scanner::new(map_buffer).try_fold(
-    WeatherStationTable::with_hasher(BuildStringHash),
+    Box::new(WeatherStationTable::with_hasher(BuildStringHash)),
     |mut map, (station, temp)| -> BarseResult<_> {
       map.add_reading(station, temp);
       Ok(map)
