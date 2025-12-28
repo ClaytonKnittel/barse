4cb24bce09610900583540b25fd98f6904de8b7f
Clayton Knittel <cknit1999@gmail.com>
2025-12-27 21:01:14 -0800

    unaligned read checks

--- src/util.rs
+++ src/util.rs
@@ -22,7 +22,16 @@ pub fn unlikely(b: bool) -> bool {
   }
 }
 
-pub fn unaligned_u64_read_would_cross_page_boundary(start_ptr: *const u8) -> bool {
+fn unaligned_read_would_cross_page_boundary<const READ_SIZE: usize>(start_ptr: *const u8) -> bool {
   const PAGE_SIZE: usize = 4096;
-  (start_ptr as usize) % PAGE_SIZE > PAGE_SIZE - (u64::BITS as usize / 8)
+  (start_ptr as usize) % PAGE_SIZE > PAGE_SIZE - READ_SIZE
+}
+
+pub fn unaligned_u64_read_would_cross_page_boundary(start_ptr: *const u8) -> bool {
+  unaligned_read_would_cross_page_boundary::<8>(start_ptr)
+}
+
+#[cfg(target_feature = "avx2")]
+pub fn unaligned_m256i_read_would_cross_page_boundary(start_ptr: *const u8) -> bool {
+  unaligned_read_would_cross_page_boundary::<32>(start_ptr)
 }
