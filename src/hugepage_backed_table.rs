b6720bf24a3ad18d84217815a0a6ce0826cfbcb7
Clayton Knittel <cknit1999@gmail.com>
2026-01-04 21:11:30 -0800

    comment hugepage table

--- src/hugepage_backed_table.rs
+++ src/hugepage_backed_table.rs
@@ -6,11 +6,18 @@ use crate::error::BarseResult;
 
 pub const HUGEPAGE_SIZE: usize = 2 * 1024 * 1024;
 
+/// A trait for objects which can be initialized from zero-initialized memory.
+/// Implementers may assume `self` references zero-initialized memory.
 pub trait InPlaceInitializable {
+  /// Initialize `self` from zero-initialized bytes spanning
+  /// `std::mem::size_of::<Self>()` bytes.
   fn initialize(&mut self);
 }
 
+/// An array of `T`s with constant `SIZE` elements allocated from `mmap`,
+/// backed by hugepages on systems that support it.
 pub struct HugepageBackedTable<T, const SIZE: usize> {
+  /// The mmapped region of `SIZE` elements of type `T`.
   elements: MmapMut,
   _phantom: PhantomData<T>,
 }
@@ -34,19 +41,23 @@ impl<T: InPlaceInitializable, const SIZE: usize> HugepageBackedTable<T, SIZE> {
 }
 
 impl<T, const SIZE: usize> HugepageBackedTable<T, SIZE> {
+  /// Returns a pointer to the start of the table.
   fn elements_ptr(&self) -> *const T {
     self.elements.as_ptr() as *const T
   }
 
+  /// Returns a mut pointer to the start of the table.
   fn mut_elements_ptr(&mut self) -> *mut T {
     self.elements.as_mut_ptr() as *mut T
   }
 
+  /// Returns a reference to the element at position `index` in the table.
   pub fn entry_at(&self, index: usize) -> &T {
     debug_assert!(index < SIZE);
     unsafe { &*self.elements_ptr().add(index) }
   }
 
+  /// Returns a mutable reference to the element at position `index` in the table.
   pub fn entry_at_mut(&mut self, index: usize) -> &mut T {
     debug_assert!(index < SIZE);
     unsafe { &mut *self.mut_elements_ptr().add(index) }
